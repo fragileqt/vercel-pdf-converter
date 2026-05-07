a06c8ef416369c188caa09df9b72f56642bb38de
Lukáš Zátek <lukaszatek@Lukass-MacBook-Pro.local>
2026-05-07 12:07:50 +0200

    fix: use raw chromium archive url

--- service/convert.js
+++ service/convert.js
@@ -2,7 +2,7 @@ import chromium from '@sparticuz/chromium-min'
 import puppeteer from 'puppeteer-core'
 
 const chromiumPackUrl = process.env.CHROMIUM_PACK_URL ||
-	'https://github.com/gabenunez/puppeteer-on-vercel/raw/refs/heads/main/example/chromium-dont-use-in-prod.tar'
+	'https://raw.githubusercontent.com/gabenunez/puppeteer-on-vercel/refs/heads/main/example/chromium-dont-use-in-prod.tar'
 
 let cachedExecutablePath = null
 let executablePathPromise = null
